9f91d6f637e82e6ee64a4517ee296b36be00c646
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2022-02-23 14:07:40 -0500

    fixed: readme typo

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 # Simple Interpreter for a toy language
 
-### Currently only supports +/- arithmetic of any sized digit & write space.
+### Currently, only supports +/- arithmetic of any sized digit & white space.
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String text = "3- 50";
+        String text = "3- 500";
         Interpreter interpreter = new Interpreter(text);
 
         int result = interpreter.expr();
